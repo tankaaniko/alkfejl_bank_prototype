18f28e6e5c59e8c07999b94eb3ed482bf0ad24ae
Becse Kolos <becsekol@gmail.com>
2017-12-04 20:00:32 +0100

    login javitgatasok

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Client } from "../client";
 import { AuthService } from "../auth.service";
 import { Router } from "@angular/router";
@@ -10,12 +10,16 @@ import { Router } from "@angular/router";
 })
 export class LoginComponent implements OnInit {
 
-  model: Client = new Client();
+  //model: Client = new Client();
+
+  @Input() client: Client;
 
   constructor(
     private authService : AuthService,
     private router : Router
-  ) { }
+  ) { 
+    this.client = new Client();
+  }
 
   ngOnInit() {
   }
@@ -26,10 +30,10 @@ export class LoginComponent implements OnInit {
       return;
     }
 
-    this.authService.login(this.model);
+    this.authService.login(this.client);
     if(this.authService.isLogged()){
       console.log("Sikeres bejelentkezés.");
-      this.router.navigate(["https://localhost:4200/transactions"])
+      //this.router.navigate(["https://localhost:4200/transactions"])
     }
 
   }
